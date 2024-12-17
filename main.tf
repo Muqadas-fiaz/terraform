ed73be4a2b24c2893ce7fd103417237444558ff3
Muqadas-fiaz <117225840+Muqadas-fiaz@users.noreply.github.com>
2024-12-17 13:47:13 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
-output "vm_public_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS_JSON
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11-bullseye-v20210916"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Assigns a public IP address
+    }
+  }
 }
